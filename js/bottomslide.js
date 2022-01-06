66df50b3c1eb3dc4ee95b175da9720114f16abd9
yjh4507 <yyjh4507@naver.com>
2022-01-06 17:21:04 +0900

    2022-01-06

--- js/bottomslide.js
+++ js/bottomslide.js
@@ -1,10 +1,12 @@
+var w=window.innerWidth;
+
 $('.prev').click(function () {
     $('.slide li:last').prependTo('.slide');
-    $('.slide').css('margin-left', -1150);
+    $('.slide').css({marginLeft: -w});
     $('.slide').stop().animate({ marginLeft: 0 }, 800)
 });
 $('.next').click(function () {
-    $('.slide').stop().animate({ marginLeft: -1150 }, 800, function () {
+    $('.slide').stop().animate({marginLeft: -w}, 800, function () {
         $('.slide li:first').appendTo('.slide');
         $('.slide').css({ marginLeft: 0 });
     });
